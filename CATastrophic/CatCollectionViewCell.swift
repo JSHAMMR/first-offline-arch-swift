f10cf09f6e623128fdd38a74c899f60f3b68eefc
Jamil Shamar <gamilaliqaid.shamar@xchanging.com>
2019-11-24 16:56:37 +0800

    overlay

---
+++ CATastrophic/CatCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CatCollectionViewCell.swift
+//  CATastrophic
+//
+//  Created by Gamil Ali Qaid Shamar on 23/11/2019.
+//  Copyright © 2019 Jamil. All rights reserved.
+//
+
+import UIKit
+
+class CatCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var overlay: UIImageView!
+
+    @IBOutlet weak var title: UILabel!
+       
+}
---
+++ CATastrophic/UIViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController.swift
+//  CATastrophic
+//
+//  Created by Gamil Ali Qaid Shamar on 24/11/2019.
+//  Copyright © 2019 Jamil. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    
+}
